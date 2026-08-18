c609099cd0d34ffdda2379be72424ce4eccb8b1b
jinks22 <121667754+jinks22@users.noreply.github.com>
2026-08-18 14:12:07 +0800

    update domain in Cors header

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,2 +1,2 @@
 //export const BASE_URL = "http://127.0.0.1:8000";
-export const BASE_URL = "https://full-stack-training-three.vercel.app";
+export const BASE_URL = "https://fullstacktrainingapi.onrender.com";
